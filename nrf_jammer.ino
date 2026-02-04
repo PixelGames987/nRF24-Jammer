a196df5623a36df728acf888c663ec3494bf8716
Krzysztof Kuczewski <92270732+PixelGames987@users.noreply.github.com>
2026-02-04 20:47:20 +0100

    Made the nrf jam all bluetooth channels

--- nrf_jammer.ino
+++ nrf_jammer.ino
@@ -15,7 +15,7 @@ byte bluetooth_channels[] = {2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
                              34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
                              50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65,
                              66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80};
-byte* current_channels = ble_adv_priority;
+byte* current_channels = bluetooth_channels;
 uint8_t channel_count = sizeof(ble_adv_priority);
 volatile uint8_t current_channel_index = 0;
 
@@ -55,7 +55,6 @@ void setup() {
     Serial.print(current_channels[i]);
     Serial.print(" ");
   }
-  delay(1000);
   Serial.println("Starting hopping...");
   digitalWrite(LED_PIN, HIGH);
 }
