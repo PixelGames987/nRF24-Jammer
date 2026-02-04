7c8a2d54da06853794cd6a29b73e8a56ed0c71f1
Krzysztof Kuczewski <92270732+PixelGames987@users.noreply.github.com>
2026-02-04 20:53:38 +0100

    Reverted back to jamming only ble adv channels

--- nrf_jammer.ino
+++ nrf_jammer.ino
@@ -15,7 +15,7 @@ byte bluetooth_channels[] = {2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
                              34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
                              50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65,
                              66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80};
-byte* current_channels = bluetooth_channels;
+byte* current_channels = ble_adv_priority;
 uint8_t channel_count = sizeof(ble_adv_priority);
 volatile uint8_t current_channel_index = 0;
 
