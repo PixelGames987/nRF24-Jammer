cf5f60cc7a09bfaad9f8fc6ee992148700663841
Krzysztof Kuczewski <92270732+PixelGames987@users.noreply.github.com>
2026-02-06 15:29:42 +0100

    Better channel spectrum coverage

--- nrf_jammer.ino
+++ nrf_jammer.ino
@@ -8,15 +8,15 @@ RF24 radio(CE_PIN, CSN_PIN);
 byte ble_channels[] = {2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
                        22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41};
 // BLE advertising priority channels
-byte ble_adv_priority[] = {37, 38, 39, 1, 2, 3, 25, 26, 27, 79, 80, 81};
+byte specific_channels[] = {37, 38, 39, 1, 2, 3, 6, 25, 27, 50, 75, 80};
 // Full Bluetooth Classic
 byte bluetooth_channels[] = {2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17,
                              18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
                              34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
                              50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65,
                              66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80};
-byte* current_channels = ble_adv_priority;
-uint8_t channel_count = sizeof(ble_adv_priority);
+byte* current_channels = specific_channels;
+uint8_t channel_count = sizeof(specific_channels);
 volatile uint8_t current_channel_index = 0;
 
 unsigned long lastBlinkTime = 0;
