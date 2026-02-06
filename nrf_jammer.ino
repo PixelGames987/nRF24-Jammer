eea4c6086081bc54be1750069175b5e4f433e1d4
Krzysztof Kuczewski <92270732+PixelGames987@users.noreply.github.com>
2026-02-06 18:23:09 +0100

    Fixed channel selection

--- nrf_jammer.ino
+++ nrf_jammer.ino
@@ -8,7 +8,7 @@ RF24 radio(CE_PIN, CSN_PIN);
 byte ble_channels[] = {2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
                        22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41};
 // BLE advertising priority channels
-byte specific_channels[] = {37, 38, 39, 1, 2, 3, 6, 25, 27, 50, 75, 80};
+byte specific_channels[] = {37, 38, 39, 1, 2, 3, 5, 25, 27, 50, 70, 80};
 // Full Bluetooth Classic
 byte bluetooth_channels[] = {2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17,
                              18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
